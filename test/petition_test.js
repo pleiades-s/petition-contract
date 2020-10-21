b36e179cddceedefca7f99f4c946396572807dd7
pleiades-s <rudobco@g.skku.edu>
2020-10-22 02:42:00 +0900

    update

--- test/petition_test.js
+++ test/petition_test.js
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 const Petiton = artifacts.require("Petition");
 
-// Account
-const contractAddress = "0x390D07B2Af175087F4B91AdA412916Afe898A0B3"; // contract address
-const goverment = "0x4016c8Da10cb66AEB15dE2F012391f52F6946C05"; // 정부
-const jury = "0x1306D18c1dB7097fDd2E6FCAB9F055120DF816F0"; // 판정단
-const user = "0xA0906De8176B1C19746b30d2Ed7FDc8c8626aB66"; // 일반 사용자
+// Account in Ganache
+const contractAddress = ""; // contract address
+const goverment = ""; // 정부
+const jury = ""; // 판정단
+const user = ""; // 일반 사용자
 
 contract('Petition', () => {
 
@@ -36,16 +35,3 @@ contract('Petition', () => {
     // 7. 정부가 답변하기 -> 글 리스트 한번 확인해보기
     // 8. 판정단 List 보기
 })
-
-
-=======
-
-
-// 1. 판정단 신청하기
-// 2. 청원글 작성하기
-// 3. 청원 block 하기
-// 4. 청원글 투표하기
-// 5. 청원글 List 보기
-// 6. 판정단 투표하기 / 중복투표 하기
-// 7. 판정단 List 보기
->>>>>>> 18c42ed45ff91c7568255f5fd7d5c8757710a8be
